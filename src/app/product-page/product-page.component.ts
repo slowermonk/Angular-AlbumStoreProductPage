74364dde7188e8b7f092d4f8d78afa4a43f300c5
slowermonk <abdallahsalimhq@gmail.com>
2020-10-14 22:07:03 +0300

    new data

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService} from '../product.service';
+
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
+albumInfo;
 
   constructor() { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe((response)=> 
+    this.albumInfo = response)
   }
 
 }
